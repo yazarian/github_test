2cc4ef67b8f0fcba8c51371e22eca771c17fa46a
Yavar Azar <y_azari@hotmail.com>
2018-06-28 10:26:46 +0200

    premiere version du projet

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GIT testt</title>
+</head>
+<body>
+    <h1> Vivement le mechoui ce midi!</h1>
+</body>
+</html>
