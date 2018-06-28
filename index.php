4b5285e1181616a9b1c282a8e3925c3a7674b708
Yavar Azar <y_azari@hotmail.com>
2018-06-28 14:14:27 +0200

    ajout background

--- index.php
+++ index.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>GIT testt</title>
-</head>
+    <title>GIT test</title>
+
+<style>
+body
+{ background: yellow;
+}
+    h1{ 
+        position: absolute;
+        top:45%;
+        left:45%;
+        color: blue;
+    }
+    </style>
+    </head>
 <body>
-    <h1> Vivement le mechoui ce midi!</h1>
-</body>
+ <h1> vivement le mechoui ce midi </h1>
+ </body>
+
 </html>
